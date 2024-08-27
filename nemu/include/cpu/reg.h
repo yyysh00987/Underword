79699d21f481c7763a338e4b1d44c33ea30358c3
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-27 18:24:38 +0800

    >  compile NEMU 3023244362 root Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:24:38 up  1:09,  1 user,  load average: 0.15, 0.12, 0.09 1029e06de4b65301d677845d79ce3670468fef67

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,20 +14,25 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-     struct {
+
+
+
+
+typedef struct{
+	swaddr_t eip;//pc
+
+	union{
+	union{
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
-     } gpr[8];
+     } gpr[8]; 
 
-     /* Do NOT change the order of the GPRs' definitions. */
+	struct{uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};//reg*8
 
-     uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	};
 
-     swaddr_t eip;
-     
-     union {
+	 union {
 		struct {
 			uint32_t CF		:1;
 			uint32_t pad0	:1;
@@ -48,8 +53,8 @@ typedef struct {
 		};
 		uint32_t val;
 	} eflags;
-
 } CPU_state;
+	
 
 extern CPU_state cpu;
 
