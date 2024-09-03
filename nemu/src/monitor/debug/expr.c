01fe06f4ee6c3514586bd826333fc41f6ce3563d
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 15:28:41 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:28:41 up  4:59,  1 user,  load average: 0.41, 0.17, 0.07 b7f32e706fe6824e7c32c5f6b3d5ebf915dad1cc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,14 +7,11 @@
 #include <regex.h>
 
 enum {
-	NOTYPE = 256, EQ,
-	DECNUM,HEXNUM,REG,
-	NEQ,
-	AND,
-	OR,
-	NEG,
-	REF,
-
+	NOTYPE = 256, EQ, NEQ, LEQ, GEQ,
+	AND, OR,
+	NEG, REF,
+	DECNUM, HEXNUM,
+	REGNAME,
 	/* TODO: Add more token types */
 
 };
@@ -28,25 +25,28 @@ static struct rule {
 	 * Pay attention to the precedence level of different rules.
 	 */
 
-	{"0x[0-9a-fA-F]{1,10}", HEXNUM},
-	{"[0-9]{1,10}", DECNUM},
-	{"\\$[a-z]{2,3}", REG},
 	{" +",	NOTYPE},				// spaces
 	{"\\+", '+'},					// plus
-	{"\\-", '-'},                  
-	{"\\*", '*'},					
-	{"\\/", '/'},					
-	{"\\%", '%'},					
-	{"\\(", '('},					// left 
-	{"\\)", ')'},					// right 
-	{"==", EQ},						// equal
+	{"\\-", '-'},                                   // minus
+	{"\\*", '*'},					// multiplication
+	{"\\/", '/'},					// division
+	{"\\%", '%'},					// modulo
+	{"\\(", '('},					// left backet
+	{"\\)", ')'},					// right backet
+
+	{"==", EQ},						// equal	
+	{"<=", LEQ},
+	{">=", GEQ},
+	{">", '>'},
+	{"<", '<'},
 	{"!=", NEQ},
 	{"\\=", '='},
 	{"\\!",'!'},
 	{"\\&\\&",AND},
 	{"\\|\\|",OR},
-	
-
+	{"0x[0-9a-fA-F]{1,10}", HEXNUM},
+	{"[0-9]{1,10}", DECNUM},
+	{"\\$[a-z]{2,3}", REGNAME},
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -82,7 +82,7 @@ static bool make_token(char *e) {
 	int position = 0;
 	int i;
 	regmatch_t pmatch;
-	
+
 	nr_token = 0;
 
 	while(e[position] != '\0') {
@@ -99,30 +99,44 @@ static bool make_token(char *e) {
 				 * to record the token in the array `tokens'. For certain types
 				 * of tokens, some extra actions should be performed.
 				 */
-
-					if (rules[i].token_type == NOTYPE)
-						break;
+				if(rules[i].token_type == NOTYPE)
+					break;
 				int ret;
-
 				switch(rules[i].token_type) {
-					//先处理三种非符号的情况1十进制数2十六进制数3寄存器
-					//不仅需要知道type还需要知道具体值，所以存到str里面
-				case DECNUM:
-				case HEXNUM:
-					ret = sprintf(tokens[nr_token].str, "%.*s", substr_len, substr_start);
-					Assert(ret == substr_len, "Numeric constant too large.");
-					break;
-				case REG:
-					sprintf(tokens[nr_token].str, "%.*s", substr_len - 1, substr_start + 1);
-					break;
-				case '+':case '-':case '*':case '/':case '%':
-				case '(':case ')':case '=':case '<':case '>':
-				case '!':case AND:case OR:case NEG:case REF:
-				case EQ:case NEQ:
-					break;
-					default: panic("please implement me");
+					case DECNUM:
+					case HEXNUM:
+						ret = sprintf(tokens[nr_token].str, "%.*s", substr_len, substr_start);
+						Assert(ret == substr_len, "Numeric constant too large.");
+						break;					
+					case REGNAME:
+						sprintf(tokens[nr_token].str, "%.*s", substr_len - 1, substr_start + 1);
+						break;	
+					case '+':
+					case '-':
+					case '*':
+					case '/':
+					case '%':
+					case '(':
+					case ')':
+					case '=':
+					case '<':
+					case '>':
+					case '!':
+					case AND:
+					case OR:
+					case NEG:
+					case REF:
+					case EQ:
+					case NEQ:
+					case LEQ:
+					case GEQ:
+						break;
+					default: 
+						panic("please implement me");
 				}
-				tokens[nr_token++].type = rules[i].token_type;
+				tokens[nr_token].type = rules[i].token_type;
+				nr_token++;
+
 				break;
 			}
 		}
@@ -131,215 +145,182 @@ static bool make_token(char *e) {
 			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
 			return false;
 		}
-		if (nr_token > 12800) {
-			printf("The expression is too long:\n %s.\n", e);
+		if(nr_token > 12800){
+			printf("The expression is too long:\n %s.\n",e);
 			return false;
-		}
+}
 	}
 
 	return true; 
 }
-
-//初代：但是复杂表达式可能出错
-/*bool check_parenthese(int l,int r){ 
-	//得到括号匹配并且可以计算的式子，可能外面大括号，或者其中小括号
-	if(tokens[l].type!='('||tokens[r].type!=')') {
-		return 0;
-		} 
-		else{
-	while(l<=r){ 
-		if(tokens[l].type=='(')
-		{
-			if(tokens[r].type==')'){
-				l++,r--;
-				continue;
-			}
-			r--;
-		}
-		else if (tokens[l].type==')') return 0;
-		else l++;
-	}
-	return true;
-}
-}*/
-
-static bool check_parentheses(int p, int q) {
-	if (p > q) {
-		return 0;
-	}
-	int i, left_num = 0;
-	for (i = p; i <= q; i++) {
-
-		if (tokens[i].type == '(')
+static bool check_parentheses(int p,int q){
+	if(p > q)return 0;
+	int i,left_num=0;
+	for(i = p;i<=q;i++){
+		if(tokens[i].type=='(')
 			left_num++;
-		else if (tokens[i].type == ')') {
-
-			if (left_num > 0)
-
+		else if(tokens[i].type==')'){
+			if(left_num > 0)
 				left_num--;
-			else
-
+			else 
 				return false;
 		}
 	}
-	return left_num == 0;
+	return left_num==0;
 }
+static int computing_priority(int operator){
+	switch(operator){
+		case '+':
+		case '-':
+			return 4;
+		case '*':
+		case '/':
+		case '%':
+			return 3;
+		case '=':
+			return 14;
+		case '<':
+		case '>':
+		case LEQ:
+		case GEQ:
+			return 6;
+		case EQ:
+		case NEQ:
+			return 7;
 
-
-static int prec(int operator) {
-	//	由符号优先级
-	switch (operator) {
-	case '+':case '-':return 4;
-	case '*':case '/':case '%':	return 3;
-	case '=':	return 14;
-	case EQ:case NEQ:return 7;
-	case AND:
-		return 11;
-	case OR:
-		return 12;
-	case '!':case NEG:case REF:
-		return 2;
-
-	case '(':
-	case ')':
-		return 1;
-
-	default:
-		return -1;
-	}
-	
-	return -1;
-
-}
-
-static uint32_t do_operate(uint32_t a, uint32_t b, int operator) {
-	switch (operator) {
-	case '+':
-		return a + b;
-	case '-':
-		return a - b;
-	case '*':
-		return a * b;
-	case '/':
-		return a / b;
-	case '%':
-		return a % b;
-	case '=':
-		return a = b;
-	case '<':
-		return a < b;
-	case '>':
-		return a > b;
-	case AND:
-		return a && b;
-	case OR:
-		return a || b;
-	case EQ:
-		return a == b;
-	case NEQ:
-		return a != b;
-	default:
-		return -1;
+		case AND:
+			return 11;
+		case OR:
+			return 12;
+		case '!':
+		case NEG:
+		case REF:
+			return 2;
+		case '(':
+		case ')':
+			return 1;
+		default:
+			return -1;
 	}return -1;
 
 }
-
-static uint32_t eval(int p, int q, bool* success) {
-		uint32_t val;
-	if (p > q) {
-		panic("Bad Expression");
-		*success = false;
-	}
-	else if (p == q) {
-		//10 16 reg
-		switch (tokens[p].type) 
-	{
-		case DECNUM:
-			sscanf(tokens[p].str, "%u", &val);
-			//用sscanf将str中的数据格式化成val
-			return val;
-		case HEXNUM:
-			sscanf(tokens[p].str, "%x", &val);
-			return val;
-		case REG:
-			if (!strcmp(tokens[p].str, "eip")) return cpu.eip;
-
-				//把eip从寄存器中单独拿出来
-				//当strcmp==0时两个相同
-				
-			int i;
-			for (i = 0; i < 8; i++) {
-				if (!strcmp(regsl[i], tokens[p].str))	return reg_l(i);
-				if (!strcmp(regsw[i], tokens[p].str))	return reg_w(i);
-				if (!strcmp(regsb[i], tokens[p].str))  return reg_b(i);
-			}
+static uint32_t do_operate(uint32_t a, uint32_t b, int operator){
+	switch(operator){
+		case '+':
+			return a + b;
+		case '-':
+			return a - b;
+		case '*':
+			return a * b;
+		case '/':
+			return a / b;
+		case '%':
+			return a % b;
+		case '=':
+			return a = b;
+		case '<':
+			return a < b;
+		case '>':
+			return a > b;
+		case AND:
+			return a && b;
+		case OR:
+			return a || b;
+		case EQ:
+			return a == b;
+		case NEQ:
+			return a != b;
+		case LEQ:
+			return a <= b;
+		case GEQ:
+			return a >= b;
 		default:
-			panic("Bad Expression");
-			*success = false;
-			break;
-		}
-	}
-	else if (tokens[p].type == '(' && tokens[q].type == ')' && check_parentheses(p + 1, q - 1)) {
-		//最外侧括号匹配并且内层括号合肥
-		return eval(p + 1, q - 1, success);
+			return -1;
+	}return -1;
 
-	}
-	else {
+}
+static uint32_t eval(int p,int q,bool *success){
+	uint32_t val;
+	if(p > q){
+		panic("Bad Expression");
+		*success = false;	
+	}else if(p == q){
+		switch(tokens[p].type){
+			case DECNUM:
+				sscanf(tokens[p].str,"%u",&val);
+				return val;
+			case HEXNUM:
+				sscanf(tokens[p].str,"%x",&val);
+				return val;
+			case REGNAME:
+				if(!strcmp(tokens[p].str,"eip"))
+					return cpu.eip;
 
+				int i;
+				for(i = 0;i < 8;i++){
+					if(!strcmp(regsl[i],tokens[p].str))
+						return reg_l(i);
+					if(!strcmp(regsw[i],tokens[p].str))
+						return reg_w(i);
+					if(!strcmp(regsb[i],tokens[p].str))
+						return reg_b(i);
+				}
+			default:
+				//	printf("p=%d\n",p);
+				panic("Bad Expression");
+				*success = false; 
+				break;
+		}	
+	}
+	else if(tokens[p].type=='('&&tokens[q].type==')'&&check_parentheses(p + 1,q - 1)){
+		return eval(p + 1,q - 1,success);
+	}
+	else{
 		int i;
-		int panum = 0;
-		int dom_op = 0, dom_op_priority = -1;
-		for (i = p; i <= q; i++) {
-			if (tokens[i].type == '(')
-				panum++;
-			else if (tokens[i].type == ')')
-				panum--;
-			else if (!panum) {
-				int priority = prec(tokens[i].type);//确定当前type优先级
-				if (priority >= dom_op_priority) { 
-					dom_op = i;
+		int left_num = 0;
+		int dom_op = 0,dom_op_priority = -1;
+		for(i = p;i <= q; i++){
+			if(tokens[i].type == '(')
+				left_num++;
+			else if(tokens[i].type == ')')
+				left_num--;
+			else if(!left_num){
+				int priority=computing_priority(tokens[i].type);		
+				if(priority>=dom_op_priority){
+					dom_op=i;
 					dom_op_priority = priority;
 				}
 			}
-		}if (dom_op_priority == 2) {
-			//考虑 - ！ 
-			//REF->SWADDR
-			val = eval(p + 1, q, success);
-			if (*success) {
-				switch (tokens[p].type) {
-				case NEG:
-					return -val;
-				case REF:
-					return swaddr_read(val, 4);
-				case '!':
-					return !val;
-				default:
-					break;
+		}if(dom_op_priority == 2){
+			val = eval(p+1 , q,success);
+			if(*success){
+				switch(tokens[p].type){
+					case NEG:
+						return -val;
+					case REF:
+						return swaddr_read(val,4);
+					case '!':
+						return !val;
+					default:
+						break;
 				}
 			}
 			panic("Bad Expression");
-			*success = false;
+			*success = false; 
 
-		}
-		else {
+		}else{
 
-			uint32_t div1 = eval(p, dom_op - 1, success);
-			if (*success == false) { return -1; }
-
-			uint32_t div2 = eval(dom_op + 1, q, success);
-			if (*success == false) { return -1; }
-
-			Assert(!(tokens[dom_op].type == '/' && div2 == 0), "divide by 0!");
-
-			val = do_operate(div1, div2, tokens[dom_op].type);
+			uint32_t lval = eval(p,dom_op-1,success);
+			if(*success == false){return -1;}
+			uint32_t rval = eval(dom_op + 1, q, success);
+			if(*success == false){return -1;}
+			Assert(!(tokens[dom_op].type == '/' && rval == 0), "divide by 0!");
+			val = do_operate(lval, rval, tokens[dom_op].type);
 			return val;
 		}
 	}
 	return -1;
 }
-
-
-
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
 		*success = false;
@@ -347,27 +328,19 @@ uint32_t expr(char *e, bool *success) {
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
-	if (check_parentheses(0, nr_token - 1)) {
-
+	if(check_parentheses(0,nr_token-1)){
 		int i;
-		for (i = 0; i < nr_token; i++) {
+		for(i = 0;i < nr_token;i ++){
 			// recognize the unary operator
-			if (tokens[i].type == '-' && (!i || prec(tokens[i - 1].type) > 1))
+			if(tokens[i].type == '-'&&(!i||computing_priority(tokens[i-1].type)>1))
 				tokens[i].type = NEG;
-
-
-			else if (tokens[i].type == '*' && (!i || prec(tokens[i - 1].type) > 1))
+			else if(tokens[i].type == '*'&&(!i||computing_priority(tokens[i-1].type)>1))
 				tokens[i].type = REF;
 		}
-
-		uint32_t val = eval(0, nr_token - 1, success);
-		printf("The result is %d\n",val);
-		if (*success) { return val; }
+		uint32_t val = eval(0, nr_token-1, success);
+		if(*success){return val;}	
 	}
-
 	*success = false;
-
 	panic("please implement me");
 	return 0;
 }
-
