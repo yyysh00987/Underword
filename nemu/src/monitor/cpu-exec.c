66e831dac35cacc07dfef0de28f25288e395d8ad
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-07 11:12:32 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:12:32 up 4 min,  1 user,  load average: 0.14, 0.24, 0.11 86abaef073042231e5a011c9c9fc39061e14e5e1

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -38,7 +38,7 @@ void do_int3() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(volatile uint32_t n) {
-	if(nemu_state == END) { //盲猜一下END==STOP
+	if(nemu_state == END) { 
 		printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
 		return;
 	}
