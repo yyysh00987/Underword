0598391c7ff938c560bd4182e4851b5635b532de
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 16:36:02 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:36:02 up  7:34,  1 user,  load average: 0.41, 0.26, 0.22 be5a32f9f430624885377dbd1889b300f8a776c8

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,23 +1,170 @@
 #include "cpu/exec/template-start.h"
 
-#define make_jcc_helper(cc) \
-	make_helper(concat4(j, cc, _, SUFFIX)) { \
-		int len = concat(decode_si_, SUFFIX) (eip + 1); \
-		print_asm(str(concat(j, cc)) " %x", cpu.eip + op_src->val + 1 + len + (DATA_BYTE == 4)); \
-		cpu.eip += (concat(check_cc_, cc)() ? op_src->val : 0); \
-		return len + 1; \
-	}
+#define instr je
 
-make_jcc_helper(b)
-make_jcc_helper(e)
-make_jcc_helper(ne)
-make_jcc_helper(be)
-make_jcc_helper(a)
-make_jcc_helper(s)
-make_jcc_helper(ns)
-make_jcc_helper(l)
-make_jcc_helper(ge)
-make_jcc_helper(le)
-make_jcc_helper(g)
+static void do_execute(){
+   DATA_TYPE_S imm = op_src -> val;
+    print_asm("je\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.ZF == 1) cpu.eip += imm;
+}
+
+make_instr_helper(i)
+#undef instr
+/*------------------------------------------------------*/
+#define instr jbe
+static void do_execute(){
+   DATA_TYPE_S imm = op_src->val;
+   print_asm("jbe\t%x",cpu.eip + imm + DATA_BYTE + 1);
+    if(cpu.eflags.ZF == 1 || cpu.eflags.CF == 1)
+    cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+/*-------------------------------------------------------*/
+
+#define instr ja
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("ja\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.CF == 0 && cpu.eflags.ZF == 0) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jle
+static void do_execute() 
+{	
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jle\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.ZF == 1 || cpu.eflags.SF != cpu.eflags.OF) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jae
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jae\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.CF == 0) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jb
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jb\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.CF == 1) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jg
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jg\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.ZF == 0 && cpu.eflags.SF == cpu.eflags.OF) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jge
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jge\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.ZF == 1 || cpu.eflags.SF == cpu.eflags.OF) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jl
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jl\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.SF != cpu.eflags.OF) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
+#define instr jne
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jne\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.ZF == 0) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
+#define instr jno
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+	if (cpu.eflags.OF == 0)
+		cpu.eip += imm;
+	print_asm("jno\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
+#define instr jnp
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+	if (cpu.eflags.PF == 0)
+		cpu.eip += imm;
+	print_asm("jnp\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
+#define instr jns
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("jns\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.SF == 0) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
+#define instr jo
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+	if (cpu.eflags.OF == 1)
+		cpu.eip += imm;
+	print_asm("jo\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
+#define instr jp
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+	if (cpu.eflags.PF == 1)
+		cpu.eip += imm;
+	print_asm("jp\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
+#define instr js
+static void do_execute() 
+{
+	DATA_TYPE_S imm = op_src -> val;
+    print_asm("js\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+    if (cpu.eflags.SF == 1) cpu.eip += imm;
+}
+make_instr_helper(i)
+#undef instr
+/*----------------------------------------------------*/
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/jcc.h
+++ nemu/src/cpu/exec/control/jcc.h
@@ -1,24 +1,38 @@
 #ifndef __JCC_H__
 #define __JCC_H__
 
-make_helper(jb_b);
-make_helper(je_b);
-make_helper(jne_b);
-make_helper(jbe_b);
-make_helper(ja_b);
-make_helper(js_b);
-make_helper(jns_b);
-make_helper(jl_b);
-make_helper(jge_b);
-make_helper(jle_b);
-make_helper(jg_b);
+make_helper(ja_i_b);
+make_helper(ja_i_v);
+make_helper(jae_i_b);
+make_helper(jae_i_v);
+make_helper(jb_i_b);
+make_helper(jb_i_v);
+make_helper(jbe_i_b);
+make_helper(jbe_i_v);
+make_helper(je_i_b);
+make_helper(je_i_v);
+make_helper(jg_i_b);
+make_helper(jg_i_v);
+make_helper(jge_i_b);
+make_helper(jge_i_v);
+make_helper(jle_i_b);
+make_helper(jle_i_v);
+make_helper(jl_i_b);
+make_helper(jl_i_v);
+make_helper(jne_i_b);
+make_helper(jne_i_v);
+make_helper(jno_i_b);
+make_helper(jno_i_v);
+make_helper(jnp_i_b);
+make_helper(jnp_i_v);
+make_helper(jns_i_b);
+make_helper(jns_i_v);
+make_helper(jo_i_b);
+make_helper(jo_i_v);
+make_helper(jp_i_b);
+make_helper(jp_i_v);
+make_helper(js_i_b);
+make_helper(js_i_v);
 
-make_helper(je_l);
-make_helper(jne_l);
-make_helper(jbe_l);
-make_helper(ja_l);
-make_helper(jl_l);
-make_helper(jge_l);
-make_helper(jle_l);
 
 #endif
