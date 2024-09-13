470e93a60eda525b24bc6b69195ad8daedcaa66e
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 18:14:28 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:14:28 up  9:10,  1 user,  load average: 0.54, 0.28, 0.21 f2e7ac7791d16032ff0c60e013a78235e3995e35

--- nemu/src/cpu/exec/control/cmp.c
+++ nemu/src/cpu/exec/control/cmp.c
@@ -4,6 +4,7 @@
 #include "cmp-template.h"
 #undef DATA_BYTE
 
+
 #define DATA_BYTE 2
 #include "cmp-template.h"
 #undef DATA_BYTE
@@ -12,8 +13,10 @@
 #include "cmp-template.h"
 #undef DATA_BYTE
 
-make_helper_v(cmp_i2a)
-make_helper_v(cmp_i2rm)
-make_helper_v(cmp_si2rm)
+/* for instruction encoding overloading */
+
 make_helper_v(cmp_r2rm)
 make_helper_v(cmp_rm2r)
+make_helper_v(cmp_i2rm)
+make_helper_v(cmp_i2a)
+make_helper_v(cmp_si2rm)
