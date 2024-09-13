d8e4695aa72e66ad809e403e7707c67739e7ea4a
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 15:52:52 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:52:52 up  6:51,  1 user,  load average: 0.35, 0.34, 0.21 c2e5a1ed18cd983c6a61419e807688a134b68252

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -1,27 +1,47 @@
 #include "cpu/exec/helper.h"
 
-make_helper(ret) {
-	//pop caller next instr address
-	printf("1 esp:%d\n",cpu.esp);
-	swaddr_t addr = swaddr_read(cpu.esp, 4);
-	printf("2 esp:%d\n",cpu.esp);
-	printf("addr :%d\n",addr);
+// make_helper(ret) {
+// 	//pop caller next instr address
+// 	printf("1 esp:%d\n",cpu.esp);
+// 	swaddr_t addr = swaddr_read(cpu.esp, 4);
+// 	printf("2 esp:%d\n",cpu.esp);
+// 	printf("addr :%d\n",addr);
 
-	cpu.eip = addr-1 ;
+// 	cpu.eip = addr-1 ;
+// 	cpu.esp += 4;
+// 	print_asm("ret");
+// 	return 1;
+// }
+// #define instr ret
+// #define SUFFIX w
+// make_helper(ret_i_w) {
+// 	int len = decode_i_w(eip + 1);
+// 	//pop caller next instr address
+// 	swaddr_t addr = swaddr_read(cpu.esp, 4);
+// 	cpu.eip = addr - 1 - len;
+// 	cpu.esp += 4;
+// 	// pop imm16 bytes
+// 	cpu.esp += (op_src->val);
+// 	print_asm_template1();
+// 	return len + 1;
+// }
+
+
+make_helper(ret) {
+	cpu.eip = swaddr_read(cpu.esp, 4) - 1;
 	cpu.esp += 4;
+
 	print_asm("ret");
+
 	return 1;
 }
-#define instr ret
-#define SUFFIX w
-make_helper(ret_i_w) {
-	int len = decode_i_w(eip + 1);
-	//pop caller next instr address
-	swaddr_t addr = swaddr_read(cpu.esp, 4);
-	cpu.eip = addr - 1 - len;
-	cpu.esp += 4;
-	// pop imm16 bytes
-	cpu.esp += (op_src->val);
-	print_asm_template1();
-	return len + 1;
+
+make_helper(ret_i) {
+	uint16_t imm = instr_fetch(eip + 1, 2);
+	cpu.eip = swaddr_read(cpu.esp, 4) - (2 + 1);
+	cpu.esp += 4 + imm;
+
+	print_asm("ret $0x%x", imm);
+
+	return 2 + 1;
 }
