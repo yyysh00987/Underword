e80cfde263c23697f6d67fb4c6c54ee3fdcc2fb9
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 14:18:11 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:18:11 up  5:17,  1 user,  load average: 0.44, 0.39, 0.28 9d88a6626382447c4876e9ffe424b7872985e403

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -1,28 +1,25 @@
-
 #include "cpu/exec/template-start.h"
+
 #define instr add
+
 static void do_execute () {
-		DATA_TYPE ret = op_dest -> val + op_src -> val;
-		OPERAND_W(op_dest, ret);	
-		/* TODO: Update EFLAGS. */    
-		cpu.eflags.ZF = !ret;    
-		cpu.eflags.SF = ret >> ((DATA_BYTE << 3) - 1);    
-		cpu.eflags.CF = (ret < op_dest -> val);    
-		int tmp1 = (op_dest -> val) >> ((DATA_BYTE << 3) - 1);    
-		int tmp2 = (op_src -> val) >> ((DATA_BYTE << 3) - 1);    
-		cpu.eflags.OF = (tmp1 == tmp2 && tmp1 != cpu.eflags.SF);    
-		ret ^= ret >> 4;  
-		  ret ^= ret >> 2;   
-		   ret ^= ret >> 1; 
-		      ret &= 1;   
-			   cpu.eflags.PF = !ret;	
-			   print_asm_template2();}
-			   make_instr_helper(i2a);
-			   make_instr_helper(i2rm);
-			   make_instr_helper(r2rm);
-			   make_instr_helper(rm2r);
-			   #if DATA_BYTE == 2 || DATA_BYTE == 4
-			   make_instr_helper(si2rm)
-			   #endif
-               
-			   #include "cpu/exec/template-end.h"
+	DATA_TYPE result = op_dest->val + op_src->val;
+	OPERAND_W(op_dest, result);
+
+	/* TODO: Update EFLAGS. */
+	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = result < op_dest->val;
+	cpu.eflags.OF = MSB(~(op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+
+	print_asm_template2();
+}
+
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+make_instr_helper(i2rm)
+make_instr_helper(i2a)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+		
+#include "cpu/exec/template-end.h"
