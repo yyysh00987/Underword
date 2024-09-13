8b119a3c5b511567595832257748ad149b2aa09a
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 18:06:46 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:06:46 up  9:02,  1 user,  load average: 0.14, 0.17, 0.17 45c02921f299a194f375c38a23166396de97eb89

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -7,6 +7,13 @@
 #define decode_i concat(decode_i_, SUFFIX)
 #define decode_a concat(decode_a_, SUFFIX)
 #define decode_r2rm concat(decode_r2rm_, SUFFIX)
+#define decode_none concat(decode_none_, SUFFIX)
+
+make_helper(concat(decode_none_, SUFFIX)){
+	op_src->val = op_dest->val = 0;
+	op_src->size = op_dest->size = 0;
+	return 0;
+}
 
 /* Ib, Iv */
 make_helper(concat(decode_i_, SUFFIX)) {
