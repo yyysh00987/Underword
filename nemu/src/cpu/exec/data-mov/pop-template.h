0ce43a76ceec12c6eb9a12d70fe9267570a1f6ee
3023244362-Yang Xiaoyue <1548209395@qq.com>
2024-09-13 18:39:16 +0800

    pa2-1over

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,12 +3,12 @@
 #define instr pop
 
 static void do_execute(){
-	printf("esp: %d\n",cpu.esp);
+	//printf("esp: %d\n",cpu.esp);
     int val = swaddr_read(cpu.esp, DATA_BYTE);//栈顶读数据
-	printf("val: %d\n",val);
+	//printf("val: %d\n",val);
     cpu.esp += DATA_BYTE; //让指针向上移动
-	printf("data byte: %d\n",DATA_BYTE);
-	printf("esp: %d\n",cpu.esp);
+//	printf("data byte: %d\n",DATA_BYTE);
+	//printf("esp: %d\n",cpu.esp);
     OPERAND_W(op_src, val);//将读取的值 val 存储到 op_src 指定的操作数中。
     print_asm_template1();
 }
