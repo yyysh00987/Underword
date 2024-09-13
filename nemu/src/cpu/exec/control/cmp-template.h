f5c94e1f7df7d5fc1257d2046915981580f65218
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 20:45:07 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:45:07 up 10:28,  1 user,  load average: 0.16, 0.27, 0.27 9f0225d5d33dabd8e2a347470d8e99fa268d4b3

--- nemu/src/cpu/exec/control/cmp-template.h
+++ nemu/src/cpu/exec/control/cmp-template.h
@@ -2,48 +2,31 @@
 
 #define instr cmp 
 
-void update_eflags_pf_zf_sf(uint32_t a);
+static void do_execute () {
+	printf("eax: %d\n",cpu.eax);
 
-static void do_execute(){
-    DATA_TYPE a = op_dest->val;
-    DATA_TYPE b = op_src->val;
-    DATA_TYPE ans = a - b;
+	printf("ecx: %d\n",cpu.ecx);
 
-    cpu.eflags.CF = a < b;
 
-    // update_eflags_pf_zf_sf(ans);
-    const int parity_table [] = {
-        0, 1, 0, 1,
-        1, 0, 1, 0,
-        0, 1, 0, 1,
-        1, 0, 1, 0
-    };
+    printf("op_dest: %d\n",op_dest->val);
 
-    uint8_t temp = ans & 0xff;
-	cpu.eflags.PF = parity_table[temp & 0xf] ^ parity_table[temp >> 4];
-	cpu.eflags.ZF = (ans == 0);
-	cpu.eflags.SF = ans >> (DATA_BYTE * 8 - 1);
+    printf("op_src: %d\n",op_src->val);
+	DATA_TYPE result = op_dest->val - op_src->val;
 
-    int tem1 = (op_dest->val)>>((DATA_BYTE<<3)-1);
-    int tem2 = (op_src->val)>>((DATA_BYTE<<3)-1);
-    cpu.eflags.OF = (tem1 != tem2 && tem2 == cpu.eflags.SF);
-   /* cpu.eflags.SF = MSB(ans);
-    cpu.eflags.PF = get_pf(ans);
-    cpu.eflags.ZF = (ans == 0);*/
-    
+	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = result > op_dest->val;
+	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
 
-    print_asm_template2();
-
+	print_asm_template2();
 }
 
-
-#if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(si2rm)
-#endif
 
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 make_instr_helper(i2rm)
 make_instr_helper(i2a)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
 
 #include "cpu/exec/template-end.h"
