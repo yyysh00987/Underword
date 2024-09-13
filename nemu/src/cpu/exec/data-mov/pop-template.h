73ad34ea2088a8339367cc6737e93e7e855ea427
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 18:33:29 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:33:29 up  9:29,  1 user,  load average: 0.42, 0.28, 0.26 d09cf146944ec9dec0f8f7156dd43be48931881b

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,9 +3,13 @@
 #define instr pop
 
 static void do_execute(){
-    int val = swaddr_read(cpu.esp, DATA_BYTE);
-    cpu.esp += DATA_BYTE;
-    OPERAND_W(op_src, val);
+	printf("esp: %d\n",cpu.esp);
+    int val = swaddr_read(cpu.esp, DATA_BYTE);//栈顶读数据
+	printf("val: %d\n",val);
+    cpu.esp += DATA_BYTE; //让指针向上移动
+	printf("data byte: %d\n",DATA_BYTE);
+	printf("esp: %d\n",cpu.esp);
+    OPERAND_W(op_src, val);//将读取的值 val 存储到 op_src 指定的操作数中。
     print_asm_template1();
 }
 
