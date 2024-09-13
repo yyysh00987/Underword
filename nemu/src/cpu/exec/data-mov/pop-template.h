5c56717d3eacc4231ca4c333549872aa182a7d7e
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 18:21:17 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:21:17 up  9:16,  1 user,  load average: 0.46, 0.24, 0.20 85a104bc9ad7efca8c386053ec1ef2da1c27ec6

---
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute() {
+	OPERAND_W(op_src, MEM_R(cpu.esp));
+	cpu.esp += DATA_BYTE;
+	print_asm_template1();
+}
+
+make_instr_helper(r);
+
+#include "cpu/exec/template-end.h"
