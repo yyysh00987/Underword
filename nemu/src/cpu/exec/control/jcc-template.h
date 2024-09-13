6f193eb486c1631f984e00b4979bf7a07204353f
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 20:08:32 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:08:32 up  9:52,  1 user,  load average: 0.37, 0.21, 0.19 e9f521cfb2e425a9597f1e8a9062ce7c80c76ff

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -4,167 +4,171 @@
 
 static void do_execute(){
    DATA_TYPE_S imm = op_src -> val;
+   printf("\nimm:%d\n",imm);
     print_asm("je\t%x", cpu.eip + 1 + DATA_BYTE + imm);
     if (cpu.eflags.ZF == 1) cpu.eip += imm;
+    printf("\nZF:%d\n",cpu.eflags.ZF);
 }
 
 make_instr_helper(i)
-#undef instr
-/*------------------------------------------------------*/
-#define instr jbe
-static void do_execute(){
-   DATA_TYPE_S imm = op_src->val;
-   print_asm("jbe\t%x",cpu.eip + imm + DATA_BYTE + 1);
-    if(cpu.eflags.ZF == 1 || cpu.eflags.CF == 1)
-    cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
-/*-------------------------------------------------------*/
 
-#define instr ja
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("ja\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.CF == 0 && cpu.eflags.ZF == 0) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
 
-#define instr jle
-static void do_execute() 
-{	
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jle\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.ZF == 1 || cpu.eflags.SF != cpu.eflags.OF) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
+// #undef instr
+// /*------------------------------------------------------*/
+// #define instr jbe
+// static void do_execute(){
+//    DATA_TYPE_S imm = op_src->val;
+//    print_asm("jbe\t%x",cpu.eip + imm + DATA_BYTE + 1);
+//     if(cpu.eflags.ZF == 1 || cpu.eflags.CF == 1)
+//     cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*-------------------------------------------------------*/
 
-#define instr jae
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jae\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.CF == 0) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
+// #define instr ja
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("ja\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.CF == 0 && cpu.eflags.ZF == 0) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
 
-#define instr jb
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jb\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.CF == 1) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
+// #define instr jle
+// static void do_execute() 
+// {	
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jle\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.ZF == 1 || cpu.eflags.SF != cpu.eflags.OF) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
 
-#define instr jg
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jg\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.ZF == 0 && cpu.eflags.SF == cpu.eflags.OF) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
+// #define instr jae
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jae\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.CF == 0) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
 
-#define instr jge
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jge\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.ZF == 1 || cpu.eflags.SF == cpu.eflags.OF) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
+// #define instr jb
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jb\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.CF == 1) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
 
-#define instr jl
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jl\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.SF != cpu.eflags.OF) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
-#define instr jne
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jne\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.ZF == 0) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
-#define instr jno
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-	if (cpu.eflags.OF == 0)
-		cpu.eip += imm;
-	print_asm("jno\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
-#define instr jnp
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-	if (cpu.eflags.PF == 0)
-		cpu.eip += imm;
-	print_asm("jnp\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
-#define instr jns
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("jns\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.SF == 0) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
-#define instr jo
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-	if (cpu.eflags.OF == 1)
-		cpu.eip += imm;
-	print_asm("jo\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
-#define instr jp
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-	if (cpu.eflags.PF == 1)
-		cpu.eip += imm;
-	print_asm("jp\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
-#define instr js
-static void do_execute() 
-{
-	DATA_TYPE_S imm = op_src -> val;
-    print_asm("js\t%x", cpu.eip + 1 + DATA_BYTE + imm);
-    if (cpu.eflags.SF == 1) cpu.eip += imm;
-}
-make_instr_helper(i)
-#undef instr
-/*----------------------------------------------------*/
+// #define instr jg
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jg\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.ZF == 0 && cpu.eflags.SF == cpu.eflags.OF) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
+
+// #define instr jge
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jge\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.ZF == 1 || cpu.eflags.SF == cpu.eflags.OF) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
+
+// #define instr jl
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jl\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.SF != cpu.eflags.OF) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
+// #define instr jne
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jne\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.ZF == 0) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
+// #define instr jno
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+// 	if (cpu.eflags.OF == 0)
+// 		cpu.eip += imm;
+// 	print_asm("jno\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
+// #define instr jnp
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+// 	if (cpu.eflags.PF == 0)
+// 		cpu.eip += imm;
+// 	print_asm("jnp\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
+// #define instr jns
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("jns\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.SF == 0) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
+// #define instr jo
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+// 	if (cpu.eflags.OF == 1)
+// 		cpu.eip += imm;
+// 	print_asm("jo\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
+// #define instr jp
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+// 	if (cpu.eflags.PF == 1)
+// 		cpu.eip += imm;
+// 	print_asm("jp\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
+// #define instr js
+// static void do_execute() 
+// {
+// 	DATA_TYPE_S imm = op_src -> val;
+//     print_asm("js\t%x", cpu.eip + 1 + DATA_BYTE + imm);
+//     if (cpu.eflags.SF == 1) cpu.eip += imm;
+// }
+// make_instr_helper(i)
+// #undef instr
+// /*----------------------------------------------------*/
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-mov/push.h
+++ nemu/src/cpu/exec/data-mov/push.h
@@ -2,6 +2,7 @@
 #define __PUSH_H__
 
 make_helper(push_si_b);
+make_helper(push_si_v);
 
 make_helper(push_r_v);
 make_helper(push_rm_v);
