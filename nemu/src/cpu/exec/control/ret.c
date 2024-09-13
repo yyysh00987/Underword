012c9d90e3fdc720e9342614a5d1455fbbf1d3fd
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-13 15:42:07 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:42:07 up  6:40,  1 user,  load average: 0.09, 0.14, 0.11 34bd1197dbe0601e6268c65aaa6ecec63f2c24a6

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -3,7 +3,7 @@
 make_helper(ret) {
 	//pop caller next instr address
 	swaddr_t addr = swaddr_read(cpu.esp, 4);
-	cpu.eip = addr - 1;
+	cpu.eip = addr ;
 	cpu.esp += 4;
 	print_asm("ret");
 	return 1;
