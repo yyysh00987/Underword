5b8fa18a31592bde7b1808eeba939a0ccb8d09a6
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 00:50:04 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:50:04 up  3:00,  1 user,  load average: 0.52, 0.45, 0.36 14389f2f48e01a6dc8764d7709c0b343b455b9

--- nemu/src/cpu/exec/arith/sbb.h
+++ nemu/src/cpu/exec/arith/sbb.h
@@ -2,6 +2,15 @@
 #define __SBB_H__
 
 
+make_helper(sbb_rm2r_b);
+make_helper(sbb_rm2r_v);
+make_helper(sbb_r2rm_b);
+make_helper(sbb_r2rm_v);
+make_helper(sbb_i2rm_b);
+make_helper(sbb_i2rm_v);
+
+make_helper(sbb_i2a_b);
+make_helper(sbb_i2a_v);
 make_helper(sbb_r2rm_v);
 
 #endif
