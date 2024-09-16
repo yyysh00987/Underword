445b487f845c6fbbbfef37c454d85310806807a2
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-16 23:33:31 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:33:31 up  1:43,  1 user,  load average: 0.34, 0.17, 0.14 ed17d743daafc351a320ba31dcba44d9f6167c4

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -5,12 +5,14 @@
 static void do_execute () {
 	DATA_TYPE result = op_dest->val & op_src->val;
 
-	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
-	cpu.eflags.CF = cpu.eflags.OF = 0;
+        update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+        cpu.eflags.CF = cpu.eflags.OF = 0;
+
 
 	print_asm_template2();
 }
 
+make_instr_helper(i2a)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
 
