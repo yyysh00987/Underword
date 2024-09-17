54e6d0baa731da3b4169047a09b0b753c9d51971
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 16:50:26 +0800

    >  compile NEMU 3023244362 underword Linux Galaxy 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:50:26 up  1:33,  1 user,  load average: 0.53, 0.26, 0.21 778bd251a3315546fb0fbb24c8d66166a80ba03

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,22 +14,22 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
- typedef struct {
-	 swaddr_t eip;//pc
-	 union{
+typedef struct {
+	union{
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
 
-		struct{uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};//reg *8
+		/* Do NOT change the order of the GPRs' definitions. */
+		struct{
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
+	};
+	swaddr_t eip;
 
-		union{
-
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-
-		}gpr[8];
-
-	 };
-	 union {
+	union {
 		struct {
 			uint32_t CF		:1;
 			uint32_t pad0	:1;
@@ -50,8 +50,8 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 		};
 		uint32_t val;
 	} eflags;
- }CPU_state;
 
+} CPU_state;
 
 extern CPU_state cpu;
 
